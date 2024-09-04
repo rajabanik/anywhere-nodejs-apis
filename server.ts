926a8b8c595af86d21d6bd53a23e627c41d3ae51
Subarna Bhowmik <subarnabhowmik@Subarnas-MacBook-Air.local>
2024-09-04 20:34:15 +0530

    Added & enabled CORS

--- server.ts
+++ server.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import { conn } from './config/connection'; 
-import userRouter from './routes/users/userRouter'
+import userRouter from './routes/users/userRouter';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: '*', 
+  methods: ['*'], 
+  allowedHeaders: ['*'],
+  credentials: true, 
+};
+
 app.use(express.json());
+
+app.use(cors(corsOptions));
 
 app.use('/users',userRouter);
 
