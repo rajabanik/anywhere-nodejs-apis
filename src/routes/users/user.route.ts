dda0d06ff610644312a317591b06e6ab1b993a13
Subarna Bhowmik <subarnabhowmik@Subarnas-MacBook-Air.local>
2024-09-13 01:31:55 +0530

    Added src/

---
+++ src/routes/users/user.route.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import multer from "multer";
+import {
+  createUser,
+  getProfileDetails,
+  updateProfilePhoto,
+} from "../../controllers/users/user.controller";
+
+const userRouter: Router = Router();
+
+const file = multer({ storage: multer.memoryStorage() });
+
+userRouter.get("/get-profile-details", getProfileDetails);
+
+userRouter.post("/create-account", createUser);
+
+userRouter.post("/update-profile-photo", file.single("file"), updateProfilePhoto);
+
+export default userRouter;
